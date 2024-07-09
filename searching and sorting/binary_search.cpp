e97c701a612c99e12fc4a2ad92af5a67bacbf524
Kalpit Agrawal <kk.agrawal18052000@gmail.com>
2024-07-09 20:49:04 +0530

    hii

--- searching and sorting/binary_search.cpp
+++ searching and sorting/binary_search.cpp
@@ -1,11 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int func(){
+int binary(int ar[],int n,int m){
+    int start=0;
+    int last=n-1;
+    int mid=((start)+((last-start)/2));
+    
+    int count=0;
+    while(start<=last){
+        if(ar[mid]==m){
+            count++;
+        }
+        if(m>ar[mid]){
+            start=mid+1;
+        }
+        else{
+            last=mid-1;
+        }
+        mid=((start)+((last-start)/2));
+        
+    }
+    if(count==0){
+        cout<<"Element not found";
+    }
+    else{
+        cout<<"Found at "<<mid<<" index";
+    }
 }
 
 int main(){
 
-    func();
+    int arr[100];
+    int n,m;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cin>>m;
+    binary(arr,n,m);
     return 0;
 }
