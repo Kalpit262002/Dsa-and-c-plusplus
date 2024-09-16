61fed31cada6e6a0f1f44eb7c36f8109dc90b698
Kalpit Agrawal <kk.agrawal18052000@gmail.com>
2024-09-16 09:35:34 +0530

    hii

--- pointers/ques.c
+++ pointers/ques.c
@@ -1,10 +1,21 @@
+// #include<stdio.h>
+// #include<conio.h>
+// void main(){
+//     int k=4;
+//     int *const p=&k;
+//     int r=3;
+//     // p=&k;   //error here
+//     printf("%d\n",p);
+//     printf("%d",&k);
+// }
+
 #include<stdio.h>
 #include<conio.h>
-void main(){
-    int k=4;
-    int *const p=&k;
-    int r=3;
-    // p=&k;   //error here
-    printf("%d\n",p);
-    printf("%d",&k);
+int i;
+int main(){
+    if(i);
+    else{
+        printf("Else");
+    }
+    return 0;
 }
