c0797f398adc07d71c935ede50bdf8fa2024a754
Kalpit Agrawal <kk.agrawal18052000@gmail.com>
2024-10-30 22:06:35 +0530

    Macros

---
+++ extra/macro.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fi_n for(int i=0;i<n;i++)
+int func(){
+
+}   
+
+int main(){
+    int n;
+    cin>>n;
+    fi_n{
+        cout<<"Hey ";
+    }
+
+    func();
+    return 0;
+}
