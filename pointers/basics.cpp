3a3015071387b238143c4b576ac304b3bbdb6833
Kalpit Agrawal <kk.agrawal18052000@gmail.com>
2024-10-30 08:18:52 +0530

    hii

--- pointers/basics.cpp
+++ pointers/basics.cpp
@@ -9,10 +9,11 @@ int main(){
     int i=5;
     int *p=&i;
     // int k=&i; error since address can't be stored in integer variable.
-    int *j=p; 
+    int *j=p; //copy of a pointer 
     cout<<&i<<" Adsress of i"<<endl;
     cout<<p<<" printing p"<<endl;
     cout<<&p<<" printing &p"<<endl;
+    cout<<&j<<" printing &j"<<endl;
     cout<<j<<" printing j"<<endl;
     cout<<*p<<" printing *p"<<endl;
     cout<<++(*p)<<" printing ++*p"<<endl;
