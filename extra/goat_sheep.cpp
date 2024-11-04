799fa7e77fc57a1ac64b557133108d72602ec980
Kalpit Agrawal <kk.agrawal18052000@gmail.com>
2024-11-04 12:07:03 +0530

    string

---
+++ extra/goat_sheep.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int func(){
+
+}
+
+int main(){
+
+    string s1="72A3";
+    int goat=5;
+    string s2;
+    s2.push_back('2');
+    s2.push_back(goat+'0');
+    cout<<s1<<endl;
+    cout<<s2<<endl;
+    func();
+    return 0;
+}
