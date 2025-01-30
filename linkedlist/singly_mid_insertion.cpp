255059df3b80f704e2a996046e934a4de6c73a0b
Kalpit Agrawal <kk.agrawal18052000@gmail.com>
2025-01-30 12:11:48 +0530

    hii

--- linkedlist/singly_mid_insertion.cpp
+++ linkedlist/singly_mid_insertion.cpp
@@ -11,19 +11,34 @@ class node{
         this->next=NULL;
     }
 };
-void insertionAtMid(node* &head,int l,int d){
+void insertionAtBeginning(node* &head, int d){
     node* temp=new node(d);
-    node* temp2=head;
-    for(int i=1;i<l-1;i++){
-        temp2=temp2->next;
-    }
-    temp->next=temp2->next;
-    temp2->next=temp;
+    temp->next=head;
+    head=temp;
 }
 void insertionAtEnd(node* &tail,int d){
     node* temp=new node(d);
     tail->next=temp;
     tail=temp;
+}
+void insertionAtMid(node* &head,node* &tail,int l,int d){
+    if(l==1){
+        insertionAtBeginning(head,d);
+        return;
+    }
+    node* temp=new node(d);
+    node* temp2=head;
+    for(int i=1;i<l-1;i++){
+        temp2=temp2->next;
+    }
+    if(temp2->next==NULL){
+        insertionAtEnd(tail,d);
+        return;
+    }
+    else{
+    temp->next=temp2->next;
+    temp2->next=temp;
+    }
 }
 void traverse(node* &head){
     node* temp=head;
@@ -42,8 +57,10 @@ int main(){
     insertionAtEnd(tail,73);
     //before insertion at mid
     traverse(head);
-    insertionAtMid(head,4,23);
+    insertionAtMid(head,tail,4,23);
     //after insertion at mid
     traverse(head);
+    cout<<head->data<<endl;
+    cout<<tail->data<<endl;
     return 0;
 }
